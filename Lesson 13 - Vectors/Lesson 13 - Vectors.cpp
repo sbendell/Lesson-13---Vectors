83fb861a5f996f9e83d634b14cf200ad46576b0a
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-12 11:25:46 +0100

    Created 10 enemies
    Initialized 10 enemies on the stack.

--- Lesson 13 - Vectors/Lesson 13 - Vectors.cpp
+++ Lesson 13 - Vectors/Lesson 13 - Vectors.cpp
@@ -11,6 +11,17 @@ int main()
 {
 	vector<ArmedEnemy> aevector;
 
+	ArmedEnemy ae1(100, 20);
+	ArmedEnemy ae2(90, 20);
+	ArmedEnemy ae3(80, 20);
+	ArmedEnemy ae4(70, 20);
+	ArmedEnemy ae5(60, 20);
+	ArmedEnemy ae6(50, 20);
+	ArmedEnemy ae7(40, 20);
+	ArmedEnemy ae8(30, 20);
+	ArmedEnemy ae9(20, 20);
+	ArmedEnemy ae10(10, 20);
+
     return 0;
 }
 
